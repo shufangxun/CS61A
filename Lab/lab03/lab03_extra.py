0e3f7f8bbd8cd2f05027d0f1c914c37dffd735c7
shufangxun <1623062935@qq.com>
2019-01-27 22:30:49 +0800

    2019-1-27

--- Lab/lab03/lab03_extra.py
+++ Lab/lab03/lab03_extra.py
@@ -31,15 +31,15 @@ def cycle(f1, f2, f3):
     19
     """
     "*** YOUR CODE HERE ***"
-    funcs = [f1, f2, f3]
+    F = [f3, f1, f2]    # the order should be changed
     def  take(n) :
         def do(x):
             i = 1
             res = x
             while i <= n :
-                res = func[i % 3](res)
+                x = F[i % 3](x)
                 i += 1
-            return res
+            return x
         return do
     return take
 
@@ -60,9 +60,9 @@ def is_palindrome(n):
     True
     """
     x, y = n, 0
-    f = lambda: _____
+    f = lambda: x % 10 + y * 10
     while x > 0:
-        x, y = _____, f()
+        x, y = x // 10, f()
     return y == n
 
 ## More recursion practice
@@ -75,8 +75,8 @@ def skip_mul(n):
     >>> skip_mul(8) # 8 * 6 * 4 * 2
     384
     """
-    if n == 2:
-        return 2
+    if n <= 1:
+        return 1
     else:
         return n * skip_mul(n - 2)
 
@@ -91,6 +91,15 @@ def is_prime(n):
     True
     """
     "*** YOUR CODE HERE ***"
+    def take(k) :
+        while k < n :
+            if n % k == 0 :
+                return False
+            else :
+                return take(k + 1)
+        return True
+    return take(2)
+
 
 def interleaved_sum(n, odd_term, even_term):
     """Compute the sum odd_term(1) + even_term(2) + odd_term(3) + ..., up
@@ -101,6 +110,21 @@ def interleaved_sum(n, odd_term, even_term):
     29
     """
     "*** YOUR CODE HERE ***"
+    if n == 0:
+        return 0
+    elif n % 2 == 0:
+        return even_term(n) + interleaved_sum(n - 1, odd_term, even_term)
+    else:
+        return odd_term(n) + interleaved_sum(n - 1,odd_term, even_term)
+
+        # Official solution
+        """
+    def helper(term0, term1, k):
+        if k == n:
+            return term0(k)
+        return term0(k) + helper(term1, term0, k + 1)
+    return helper(odd_term, even_term, 1)
+    """
 
 def ten_pairs(n):
     """Return the number of ten-pairs within positive integer n.
@@ -113,3 +137,16 @@ def ten_pairs(n):
     6
     """
     "*** YOUR CODE HERE ***"
+    def count_digit(n, digit):
+        if n == 0:
+            return 0
+        elif n % 10 == digit:
+            return count_digit(n // 10, digit) + 1
+        else:
+            return count_digit(n // 10, digit)
+
+    if n < 10:
+        return 0
+    else:
+        return ten_pairs(n//10) + count_digit(n//10, 10 - n%10)
+
